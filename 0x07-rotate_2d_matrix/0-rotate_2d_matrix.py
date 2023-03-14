b7eba26d8de2041c72965a6ac0133ff2faa1c373
emukus <mukuna.eugene@gmail.com>
2023-03-14 20:16:13 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a 2d matrix 90° clockwise
+    Returns: nothing
+    """
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save the top left value
+            topleft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topleft
+        right -= 1
+        left += 1
