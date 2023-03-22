f9d3ea250201776d0017b52489bb17d03fabcd7c
emukus <mukuna.eugene@gmail.com>
2023-03-22 22:40:12 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that determines the fewest number of coins needed to meet a given amount total."""
+
+
+def makeChange(coins, total):
+    """Takes a list of coins and calculates how much change the total
+    will require"""
+    if total <= 0:
+        return 0
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
