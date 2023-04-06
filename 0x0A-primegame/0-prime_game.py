4b8aa1f9f9e46f181c9cf1c85ea1627e580b3787
emukus <mukuna.eugene@gmail.com>
2023-04-06 19:12:11 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,24 +6,21 @@ the README"""
 def isWinner(x, nums):
     """
     Determines the winner of a game of prime numbers.
-    
     Args:
         x (int): the number of rounds to play
         nums (list): the list of numbers n to play
-    
     Returns:
-        string: the winner of the game (Ben or Maria), or None if there is a tie
+        string: the winner of the game (Ben or Maria),
+        or None if there is a tie
     """
     if not x or not nums:
         return None
-    
+
     def is_prime(num):
         """
         Determines whether a number is prime.
-        
         Args:
             num (int): the number to check
-            
         Returns:
             bool: True if the number is prime, False otherwise
         """
@@ -33,7 +30,7 @@ def isWinner(x, nums):
             if num % i == 0:
                 return False
         return True
-    
+
     ben = 0
     maria = 0
     for num in nums:
@@ -42,8 +39,8 @@ def isWinner(x, nums):
             ben += 1
         else:
             maria += 1
-    
+
     if ben == maria:
         return None
-    
+
     return "Ben" if ben > maria else "Maria"
