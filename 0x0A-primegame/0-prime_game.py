93c17fc3e7147abeb3e55e43580271e2158c2d1f
emukus <mukuna.eugene@gmail.com>
2023-04-06 20:19:22 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,27 @@
 the README"""
 
 
+def prime_btwn(n):
+    """
+    calculate prime numbers btwn 1 and n
+    Args:
+        n (int): the number to calculate prime numbers up to
+    Returns:
+        int: the number of prime numbers btwn 1 and n
+    """
+    prime_numbers = 0
+
+    for k in range(2, n + 1):
+        is_prime = True
+        for l in range(2, k // 2 + 1):
+            if k % l == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_numbers += 1
+    return prime_numbers
+
+
 def isWinner(x, nums):
     """
     Determines the winner of a game of prime numbers.
@@ -10,37 +31,20 @@ def isWinner(x, nums):
         x (int): the number of rounds to play
         nums (list): the list of numbers n to play
     Returns:
-        string: the winner of the game (Ben or Maria),
-        or None if there is a tie
+       string: the winner of the game (Ben or Maria),
+       or None if there is a tie
     """
     if not x or not nums:
         return None
-
-    def is_prime(num):
-        """
-        Determines whether a number is prime.
-        Args:
-            num (int): the number to check
-        Returns:
-            bool: True if the number is prime, False otherwise
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5)+1):
-            if num % i == 0:
-                return False
-        return True
-
     ben = 0
     maria = 0
-    for num in nums:
-        prime_nums = sum([1 for i in range(2, num+1) if is_prime(i)])
+    for k in range(x):
+        prime_nums = prime_btwn(nums[k])
         if prime_nums % 2 == 0:
             ben += 1
         else:
             maria += 1
-
     if ben == maria:
         return None
-
-    return "Ben" if ben > maria else "Maria"
+    winner = "Ben" if ben > maria else "Maria"
+    return winner
